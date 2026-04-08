17abfb5627e6b2a083cf5c5dc59c5c27397f852c
Edvards_Malinauskas <edvard1407@gmail.com>
2026-04-08 09:36:22 +0300

    task day 07/04/26

---
+++ untitled/src/main/java/com/bootcampexcercise/modul2/VariableAssignmentActivity.java
@@ -0,0 +1,9 @@
+package com.bootcampexcercise.modul2;
+
+public class VariableAssignmentActivity {
+    public static void main(String[] args) {
+        int number = 0;
+        number = 10;
+        System.out.println(number);
+    }
+}
